a2f32edbc45a1faec2cfde3a43902ca53b440af3
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-23 15:34:25 +0100

    Root module with @Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 
+//Decorator - which is a type script feature
 @Component({
+  //Selector creates the element in the html
   selector: 'app-root',
+  //Contains a reference to our Sass file.  
   styleUrls: ['app.component.scss'],
+  // Creates a template in a string that is rendered to our index.html.
   template: `
     <div class="app">
-      Hello!
+      {{ title }}
     </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular'
+  }
 }
