996a263ea82407014f4a5615fd79506aaaa976e8
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-23 15:46:36 +0100

    Root module with @NgModule - explanation of what's going on in there

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
+// typically these are the two that you will need to include in your base ng module.
+
+// since we're rendering our app in the browser we use this BrowserModule
 import { BrowserModule } from '@angular/platform-browser';
+// Gives us directives that we'll need later.
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
+// Tells angular that appmodule is a NgModule (special module)
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
+  //register the appComponetn in the module
   declarations: [
     AppComponent
-  ]
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  //we tell the module to bootstrap this component
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
