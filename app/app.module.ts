20729d341d7356e32800263a32b39b5624a71144
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-23 15:52:35 +0100

    Bootstrapping Angular - walk through of the main.ts file

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
-// typically these are the two that you will need to include in your base ng module.
-
-// since we're rendering our app in the browser we use this BrowserModule
 import { BrowserModule } from '@angular/platform-browser';
-// Gives us directives that we'll need later.
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
-// Tells angular that appmodule is a NgModule (special module)
 @NgModule({
-  //register the appComponetn in the module
   declarations: [
     AppComponent
   ],
@@ -18,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CommonModule
   ],
-  //we tell the module to bootstrap this component
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+//Tell angular how we are going to bootstrap it, specific ways to tell it how via on server, client etc.
+//contains the client side code that processes our templates, bindings and depencies injections
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// get our created Module
 import { AppModule } from './app/app.module';
 
+// Tell our MOdule what to bootstrap.
 platformBrowserDynamic().bootstrapModule(AppModule);
