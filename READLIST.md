dd9ee724ad30b5682e72d39f0fd9956ee5630837
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-24 00:19:46 +0100

    Creating a container (smart) component

--- READLIST.md
+++ READLIST.md
@@ -1,3 +1,4 @@
 ### Reading List
 
 - what is (shadowDOM)[https://glazkov.com/2011/01/14/what-the-heck-is-shadow-dom/]?
+- (Understanding Angular Modules (NgModule) anad their scopes)[https://medium.com/@cyrilletuzi/understanding-angular-modules-ngmodule-and-their-scopes-81e4ed6f7407]
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+
 @NgModule({
     // Hold all the components relevant to this module
     declarations: [
-        
+        PassengerDashboardComponent
     ],
     imports: [
         CommonModule
+    ],
+    exports: [
+        // Export it so we can use it in our other modules
+        PassengerDashboardComponent
     ]
 })
 
-export class PassengerDashboardModule {
-
-}
+export class PassengerDashboardModule {}
