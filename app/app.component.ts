a2d428177de84ece8ef367b78607151a8f521374
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-23 18:25:32 +0100

    ngIf, * syntax and <template>

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,24 +6,34 @@ import { Component } from '@angular/core';
   // template ref - create a reference to a particular dom node.
   template: `
     <div class="app">
-      <button (click)="handleClick(username.value)">
-        Get Value
-      </button>
-      <input type="text" #username/>
-      <button (click)="handleClick(signinDate.value)">
-        Get Date
-      </button>
-      <input type="date" #signinDate />
-      <div>{{ name }}</div>
+      <input 
+        type="text"
+        [value]="name"
+        (input)="handleChange($event.target.value)"
+      />
+
+      <!-- https://www.webcomponents.org/specs -->
+      <!-- The * is syntax sugar for the template tag -->
+      <template [ngIf]="name.length > 2">
+        <div>
+          Searching for... {{ name }}
+        </div>
+      </template>
+      
+      <!-- structural directive - we can add directives to components or templates -->
+      <div *ngIf="name.length > 2">
+        Searching for... {{ name }}
+      </div>
+
     </div>
   `
 })
 
 export class AppComponent {
-  name: string = 'Alistair'
+  name: string = ''
   
-  handleClick(value: string) {
-    console.log(value);
+  handleChange(value: string) {
+    this.name = value;
   }
 
 }
