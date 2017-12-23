3c6f7fc6d70ed7d8468acec74c11b4521257d118
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-23 23:50:19 +0100

    Feature Modules with @NgModule

---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    // Hold all the components relevant to this module
+    declarations: [
+        
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+
+export class PassengerDashboardModule {
+
+}
