2ec942b71bf899d83451ebae6fff799cf2800a84
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-23 16:38:45 +0100

    Property binding - illustrating how to do one databings in components.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,24 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
+  // [] around html attribute indicates a one way databind syntax.
   template: `
     <div class="app">
-      {{ title }}
-      <div>
-        {{ numberOne + numberTwo }}
-      </div>
-      <div>
-        {{ isHappy ? ':)' : ':(' }}
-      </div>
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <img [src]="logo" />
+      <input type="text" [value]="name"/>
+      <div>{{ name }}</div>
     </div>
   `
 })
 
 export class AppComponent {
   title: string;
-  numberOne: number = 1;
-  numberTwo: number = 2;
-  isHappy: boolean = true;
+  name: string = 'Alistair';
+  logo: string = 'img/logo.svg';
   constructor() {
     this.title = 'Ultimate Angular'
   }
