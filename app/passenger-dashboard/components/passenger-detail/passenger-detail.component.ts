536fd15103c011164c7d37eb94b6e2ed0ece5b87
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-25 20:31:53 +0100

    Dynamic @Input values with *ngFor

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import {  Passenger } from '../../models/passenger.interface'
 
 @Component({
     selector: 'passenger-detail',
+    styleUrls: ['passenger-detail.component.scss'],
     template: `
-        <div>
-            Detail component
+        <span class="status" [class.checked-in]="detail.checkedIn"></span>
+        {{ detail.fullname }}
+        <div class="date">
+            Check in date: 
+            {{detail.checkedIn ? (detail.checkedInDate | date: "yMMMMd" | uppercase) : 'Not Checked In' }}
+        </div>
+        <div class="children">
+            Children: {{ detail.children?.length || 0 }}
         </div>
     `
 })
 
 export class PassengerDetailComponent {
-    constructor() {
-
-    }
+    @Input()
+    detail: Passenger;
+    constructor() {}
 }
