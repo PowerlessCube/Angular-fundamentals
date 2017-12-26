c90b536fd5c9a7977ace9c4930e7133424111567
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-26 17:17:18 +0100

    Immutable state changes

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -59,10 +59,18 @@ export class PassengerDashboardComponent implements OnInit {
     }
 
     handleEdit(event) {
-        console.log(event); //Log event to dashboard
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+            // Checks if the passenger has been edited
+            if (passenger.id === event.id) {
+                // Immutable object: returns a new passenger with edits.
+                passenger = Object.assign({}, passenger, event)
+            }
+            return passenger;
+        })
+        console.log(this.passengers);
     }
 
-    handleRemove(event) {
+    handleRemove(event: Passenger) {
         console.log(event); // Log event to dashboard.
-    }
+        this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id)}
 }
