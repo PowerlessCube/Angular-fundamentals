eb71d3e891a9330337e456169f65ec3ce8e7120a
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-12-27 22:30:53 +0100

    ngForm and ngModel

--- READLIST.md
+++ READLIST.md
@@ -4,3 +4,4 @@
 - (Understanding Angular Modules (NgModule) anad their scopes)[https://medium.com/@cyrilletuzi/understanding-angular-modules-ngmodule-and-their-scopes-81e4ed6f7407]
 - (Notes on Provider)[https://docs.angularjs.org/api/auto/service/$provide], video (Data Services and Dependencies)[https://platform.ultimateangular.com/courses/angular-fundamentals/lectures/1956135]
 - (How to set up customer Headers in Angular)[https://blog.alex-miller.co/angular/2017/05/13/default-headers-in-angular.html]: (video)[https://platform.ultimateangular.com/courses/angular-fundamentals/lectures/1956160]
+- (ngForm and ngModel guide by Todd Motto)[https://toddmotto.com/angular-2-forms-template-driven]: (video)[https://platform.ultimateangular.com/courses/angular-fundamentals/lectures/1977204]
--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -6,11 +6,26 @@ import { Passenger } from '../../models/passenger.interface';
     selector: 'passenger-form',
     styleUrls: ['passenger-form.component.scss'],
     template: `
-        <form>
-            Forms!
+        <!-- ngForm and adding noValidate so we can use Angulars validation-->
+        <form #form="ngForm" novalidate>
+            {{ detail | json }}
             <div>
-                {{ detail | json }}
+                <!-- Template driven: every input needs a name -->
+                Passenger name:
+                <input
+                    type="text"
+                    name="fullname"
+                    [ngModel]="detail?.fullname">
             </div>
+            <div>
+                Passenger ID:
+                <input
+                    type="number"
+                    name="id"
+                    [ngModel]="detail?.id">
+            </div>
+
+            {{ form.value | json }}
         </form>
     `
 })
